506d190eb7efed03f3f7a3a8a11614ef78105d42
Kenneth V. Domingo <hello@kvdomingo.xyz>
2023-01-25 15:53:04 +0800

    autodetect firebase config in prod

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,25 @@
-import { FirebaseOptions, initializeApp } from "firebase/app";
+import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const { FIREBASE_PROJECT_ID } = import.meta.env;
+let app: FirebaseApp;
 
-const firebaseConfig: FirebaseOptions = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  appId: import.meta.env.FIREBASE_APP_ID,
-  authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: `${FIREBASE_PROJECT_ID}.appspot.com`,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-};
+if (process.env.NODE_ENV !== "production") {
+  const { FIREBASE_PROJECT_ID } = import.meta.env;
+  const firebaseConfig: FirebaseOptions = {
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    appId: import.meta.env.FIREBASE_APP_ID,
+    authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: `${FIREBASE_PROJECT_ID}.appspot.com`,
+    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  };
 
-export const app = initializeApp(firebaseConfig);
+  app = initializeApp(firebaseConfig);
+} else {
+  app = initializeApp();
+}
 
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { app, db };
